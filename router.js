95ee0de57e231ae57f823a27d74dcefb94ee6f5c
Niks2288 <163541697+Niks2288@users.noreply.github.com>
2024-05-21 16:25:25 +1000

    Add files via upload

---
+++ router.js
@@ -0,0 +1,28 @@
+// router.js
+ 
+const {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} = require("./controllers/Todo");
+ 
+console.log({
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+});
+ 
+const router = require("express").Router();
+ 
+router.get("/", (req, res) => {
+  res.send("Let's build a CRUD API!");
+});
+ 
+router.get("/todos", getTodos);
+router.post("/todos", createTodo);
+router.put("/todos/:todoID", updateTodo);
+router.delete("/todos/:todoID", deleteTodo);
+ 
+module.exports = router;
